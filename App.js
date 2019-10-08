a801b91bc6beb37792ebd333f5c7333e9788c299
yykdev <assavictory@gmail.com>
2019-10-08 15:24:03 +0900

    앱 실행>로딩 프로세스 구현

--- App.js
+++ App.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+    state = {
+        loaded: false
+    };
+
+    handleError = error => {
+        console.log(error);
+    };
+
+    handleLoaded = () =>
+        this.setState({
+            loaded: true
+        });
+
+    loadAssets = async () => {
+        await Font.loadAsync({
+            ...Ionicons.font
+        });
+    };
+
+    render() {
+        const { loaded } = this.state;
+        if (loaded) {
+            return (
+                <View style={styles.container}>
+                    <Text>Open up App.js to start working on your app!</Text>
+                </View>
+            );
+        } else {
+            return (
+                <AppLoading
+                    startAsync={this.loadAssets}
+                    onFinish={this.handleLoaded}
+                    onError={this.handleError}
+                />
+            );
+        }
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    }
 });
