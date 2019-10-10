1aa985aba0af90700941a3331b68b777863f84e5
yykdev <assavictory@gmail.com>
2019-10-10 17:30:56 +0900

    TV api 컨테이너 셋팅

--- screens/TV/TVContainer.js
+++ screens/TV/TVContainer.js
@@ -1,12 +1,46 @@
 import React from "react";
 import TVPresenter from "./TVPresenter";
+import { tv } from "../../api";
 
 export default class TVContainer extends React.Component {
     state = {
-        loading: true
+        loading: true,
+        popular: null,
+        airingThisWeek: null,
+        airingToday: null,
+        error: null
     };
+
+    async componentDidMount() {
+        let popular, airingThisWeek, airingToday, error;
+
+        try {
+            ({
+                data: { results: popular }
+            } = await tv.getPopular());
+
+            ({
+                data: { results: airingThisWeek }
+            } = await tv.getAiringThisWeek());
+            ({
+                data: { results: airingToday }
+            } = await tv.getAiringToday());
+        } catch {
+            error = "Can't get Movies";
+        } finally {
+            this.setState({
+                loading: false,
+                popular,
+                airingThisWeek,
+                airingToday,
+                error
+            });
+        }
+    }
+
     render() {
-        const { loading } = this.state;
+        const { loading, popular, airingThisWeek, airingToday } = this.state;
+        console.log(loading, popular, airingThisWeek, airingToday);
         return <TVPresenter loading={loading} />;
     }
 }
