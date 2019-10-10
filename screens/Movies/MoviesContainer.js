313d4e3855eb9e8781d271f8a3370481c5250a8b
yykdev <assavictory@gmail.com>
2019-10-10 17:05:06 +0900

    Movie API 호출 구현

--- screens/Movies/MoviesContainer.js
+++ screens/Movies/MoviesContainer.js
@@ -1,12 +1,43 @@
 import React from "react";
 import MoviesPresenter from "./MoviesPresenter";
+import { movies } from "../../api";
 
 export default class MoviesContainer extends React.Component {
     state = {
-        loading: true
+        loading: true,
+        upcoming: null,
+        popular: null,
+        nowPlaying: null,
+        error: null
     };
+
+    async componentDidMount() {
+        let upcoming, popular, nowPlaying, error;
+        try {
+            ({
+                data: { results: upcoming }
+            } = await movies.getUpcoming());
+            ({
+                data: { results: popular }
+            } = await movies.getPopular());
+            ({
+                data: { results: nowPlaying }
+            } = await movies.getNowPlaying());
+        } catch (error) {
+            error = `[ Can't get Movies ] ${error}`;
+        } finally {
+            this.setState({
+                loading: false,
+                error,
+                upcoming,
+                popular,
+                nowPlaying
+            });
+        }
+    }
+
     render() {
-        const { loading } = this.state;
+        const { loading, upcoming, popular, nowPlaying } = this.state;
         return <MoviesPresenter loading={loading} />;
     }
 }
