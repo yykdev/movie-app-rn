9636b8301e991316143e506fc4b0430706ff8b80
yykdev <assavictory@gmail.com>
2019-10-10 15:32:37 +0900

    TV 스크린 Loading 인디케이터 구현

--- screens/TV/TVContainer.js
+++ screens/TV/TVContainer.js
@@ -2,7 +2,11 @@ import React from "react";
 import TVPresenter from "./TVPresenter";
 
 export default class TVContainer extends React.Component {
+    state = {
+        loading: true
+    };
     render() {
-        return <TVPresenter />;
+        const { loading } = this.state;
+        return <TVPresenter loading={loading} />;
     }
 }
--- screens/TV/TVPresenter.js
+++ screens/TV/TVPresenter.js
@@ -1,3 +1,12 @@
 import React from "react";
 import { Text } from "react-native";
-export default () => <Text>TV</Text>;
+import PropTypes from "prop-types";
+import Loader from "../../components/Loader";
+
+const TVPresenter = ({ loading }) => (loading ? <Loader /> : <Text>TV</Text>);
+
+TVPresenter.propTypes = {
+    loading: PropTypes.bool.isRequired
+};
+
+export default TVPresenter;
