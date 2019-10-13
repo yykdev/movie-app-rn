05e8b4d3dc0ddbfa676853c5f0a95488debe4d2a
yykdev <assavictory@gmail.com>
2019-10-14 00:48:51 +0900

    popular 데이터 렌더링 구현 - 인기작

--- screens/TV/TVPresenter.js
+++ screens/TV/TVPresenter.js
@@ -31,6 +31,21 @@ const TVPresenter = ({ loading, popular, airingThisWeek, airingToday }) =>
                         ))}
                 </Section>
             ) : null}
+            {popular ? (
+                <Section title="Popular">
+                    {popular
+                        .filter(tv => tv.poster_path !== null)
+                        .map(tv => (
+                            <MovieItem
+                                key={tv.id}
+                                id={tv.id}
+                                posterPhoto={tv.poster_path}
+                                title={tv.name}
+                                voteAvg={tv.vote_average}
+                            />
+                        ))}
+                </Section>
+            ) : null}
             {airingThisWeek ? (
                 <Section title="Airing Week">
                     {airingThisWeek
